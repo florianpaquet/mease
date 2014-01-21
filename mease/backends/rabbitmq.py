660eb572965dd60ea436cd825fe426d08af12f95
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-01-21 12:12:04 +0100

    Same for rabbitmq backend

--- mease/backends/rabbitmq.py
+++ mease/backends/rabbitmq.py
@@ -2,7 +2,11 @@
 import pika
 import pickle
 import logging
-from pika.adapters.tornado_connection import TornadoConnection
+
+try:
+    from pika.adapters.tornado_connection import TornadoConnection
+except ImportError:
+    raise ImportError('Missing backend dependency (pika)')
 
 from .base import BasePublisher
 from .base import BaseSubscriber
